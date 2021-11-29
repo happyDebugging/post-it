618d5c081323a11f0b5947c19837accbc27277fe
squirrels <squirrelinthewoods@protonmail.com>
2021-11-29 22:36:10 +0200

    Fixed routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { PostsComponent } from './posts/posts.component';
-import { AuthGuard } from './shared/guards/auth.guard';
-import { AuthResolver } from './shared/guards/auth.resolver';
 
 const routes: Routes = [
   {path: 'posts', component: PostsComponent, pathMatch: 'full'},
